8f1afda13ccb4e383176f7b0700f4267a742ce35
kwonhygge <kitteniso1103@gmail.com>
2020-01-18 10:51:30 +0900

    Level 5- cookies and session

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 //jshint esversion:6
 
-//새로 설치한 npm package : mogoose-encryption, dotenv, md5
+//새로 설치한 npm package : mogoose-encryption, dotenv, md5, bcrypt
+//passport passport-local passport-local-mongoose express-session
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
+//session- STEP1
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
 const app = express();
 
 // const md5 = require("md5");
 // const encrypt = require("mongoose-encryption");
-
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 
 
 app.use(express.static("public"));
@@ -21,10 +28,26 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+//session- STEP2
+app.use(session({
+    secret: "Our little secret.",
+    resave:false,
+    saveUninitialized: false
+}));
+
+//session- STEP3
+app.use(passport.initialize());
+//session- STEP4
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, 
 useUnifiedTopology: true, 
 useCreateIndex: true, 
 useFindAndModify: false });
+
+//session- STEP7
+//에러 떠서 해결해줌
+mongoose.set("useCreateIndex", true);
 
 //mongoose-encryption쓸꺼고 몽구스 스키마에 플러그인을 설치해줄 거기 때문에
 //mongoose schema 써서 필요한 세팅을 미리 해줌
@@ -33,10 +56,21 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+//session- STEP5
+userSchema.plugin(passportLocalMongoose);
+
 //gitignore 만들때, .env랑 node_modules도 ignore 해주어야 함
 // userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields: ["password"]});
 
 const User = new mongoose.model("User",userSchema);
+//session- STEP6
+//serialise할 때, 쿠키를 만드는 것
+//deserialise는 쿠키를 열람하는 것
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/",function(req,res){
     res.render("home");
@@ -46,52 +80,62 @@ app.get("/login",function(req,res){
     res.render("login");
 });
 
+//session- STEP9
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+//session- STEP11
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+
+
 app.get("/register",function(req,res){
     res.render("register");
 });
 
+//session- STEP8
 app.post("/register",function(req,res){
-
-    bcrypt.hash(req.body.password, saltRounds,function(err,hash){
-        
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save(function(err){
-            if(err){
-                console.log(err);  
-            }else{
-                res.render("secrets");
-            }
-        });
-    });
-
-    
-});
-
-app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email:username},function(err,foundUser){
+    User.register({username:req.body.username},req.body.password,function(err,user){
         if(err){
             console.log(err);
-            
+            res.redirect("/register");      
         }else{
-            if(foundUser){
-                bcrypt.compare(password,foundUser.password,function(err,result){
-                    if(result===true){
-                        res.render("secrets");
-                    }
-                });
-                    
-
-            }
-
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
     });
+});
+
+//session- STEP10
+app.post("/login",function(req,res){
+    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user,function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
+        }
+        
+        
+    });
+
 });
 
 app.listen(3000,function(){
